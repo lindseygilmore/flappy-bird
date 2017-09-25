67065d1df787b8f1ae30acf7ed5b3bf79476de52
lindseygilmore <lindseygilmore21@gmail.com>
2017-09-25 15:18:56 -0500

    rightpoint interview upload

--- main.js
+++ main.js
@@ -1,78 +1,106 @@
-var mainState = {
+// var mainState = {
 
-	preload: function() {
+// 	preload: function() {
 
-		game.load.image('pipe', 'assets/pipe.png');
-	},
+// 		game.load.image('pipe', 'assets/pipe.png');
+// 	},
 
-	create: function() {
+// 	create: function() {
 
-		this.pipes = game.add.group();
+// 		this.pipes = game.add.group();
 
-		game.stage.backgroundColor = '#71c5cf';
+// 		game.stage.backgroundColor = '#71c5cf';
 
-		game.physics.startSystem(Phaser.Physics.ARCADE);
+// 		game.physics.startSystem(Phaser.Physics.ARCADE);
 
-		this.bird = game.add.sprite(100, 245, 'bird');
+// 		this.bird = game.add.sprite(100, 245, 'bird');
 
-		game.physics.arcade.enable(this.bird);
+// 		game.physics.arcade.enable(this.bird);
 
-		this.bird.body.gravity.y = 1000;
+// 		this.bird.body.gravity.y = 1000;
 
-		var spaceKey = game.input.keyboard.addKey(
-						Phaser.Keyboard.SPACEBAR);
-		spaceKey.onDown.add(this.jump, this);
+// 		var spaceKey = game.input.keyboard.addKey(
+// 						Phaser.Keyboard.SPACEBAR);
+// 		spaceKey.onDown.add(this.jump, this);
 
-		this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
-	},
+// 		this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+// 	},
 
-	addOnePipe: function(x, y){
-		var pipe = game.add.sprite(x, y, 'pipe');
+// 	addOnePipe: function(x, y){
+// 		var pipe = game.add.sprite(x, y, 'pipe');
 
-		this.pipes.add(pipe);
+// 		this.pipes.add(pipe);
 
-		game.physics.arcade.enable(pipe);
+// 		game.physics.arcade.enable(pipe);
 
-		pipe.body.velocity.x = -200;
+// 		pipe.body.velocity.x = -200;
 
-		pipe.checkWorldBounds = true;
+// 		pipe.checkWorldBounds = true;
 
-		pipe.outOfBoundsKill = true;
-	},
+// 		pipe.outOfBoundsKill = true;
+// 	},
 
-	addRowOfPipes: function(){
-		var hole = Math.floor(Math.random() * 5) + 1;
+// 	addRowOfPipes: function(){
+// 		var hole = Math.floor(Math.random() * 5) + 1;
 
-		for (var i =-; i < 8; i++)
-			if (i != hole && i != hole + 1)
-				this.addOnePipe(400, i * 60 + 10);
-	},
+// 		for (var i = 0; i < 8; i++)
+// 			if (i != hole && i != hole + 1)
+// 				this.addOnePipe(400, i * 60 + 10);
+// 	},
 
-	update: function() {
+// 	update: function() {
 
-		if (this.bird.y < 0 || this.bird.y > 490)
-				this.restartGame();
+// 		if (this.bird.y < 0 || this.bird.y > 490)
+// 				this.restartGame();
 
-	},
+// 	},
 
-	jump: function() {
+// 	jump: function() {
 
-		this.bird.body.velocity.y = -350;
-	},
+// 		this.bird.body.velocity.y = -350;
+// 	},
 
-	restartGame: function() {
+// 	restartGame: function() {
 
-		game.state.start('main');
+// 		game.state.start('main');
+// 	}
+// };
+
+
+// var game = new Phaser.Game(400, 490);
+
+// game.state.add('main', mainState);
+
+// game.state.start('main');
+
+var i = 0;
+
+for (i=0; i<=100; i++){
+	if (i % 3 && i % 5) {
+		console.log("rightpoint");
+	}else{
+		if(i % 3 == 0){
+			console.log("right");
+		}
+		if (i % 5 == 0){
+			console.log("point");
+		}
 	}
 };
 
 
-var game = new Phaser.Game(400, 490);
 
-game.state.add('main', mainState);
 
-game.state.start('main');
 
-for (var i=1; i<arr.length; i++){
-	
-}
+
+
+
+
+
+
+
+
+
+
+
+
